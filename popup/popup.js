924d70598a8b75f8207b322742cd4beb95716df1
zodiac403 <zodiac403@gmx.de>
2018-06-01 09:50:24 +0200

    Add JS prettier.

--- popup/popup.js
+++ popup/popup.js
@@ -3,70 +3,69 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
 function handleInputEvent(event) {
-    try {
-        let value = document.getElementById("input").value;
-        let inputDefinition = getInputDefinition(value);
+  try {
+    let value = document.getElementById('input').value;
+    let inputDefinition = getInputDefinition(value);
 
-        result = convert(inputDefinition.epoch);
+    result = convert(inputDefinition.epoch);
 
-        document.getElementById('result').className = "";
-        document.getElementById('error').className = "hidden";
-        document.getElementById('input-unit').innerHTML = inputDefinition.unit;
-        document.getElementById('result-iso').innerHTML = result;
-        document.getElementById('result-epoch').innerHTML = inputDefinition.epoch;
-    } catch (error) {
-        document.getElementById('result').className = "hidden";
-        document.getElementById('error').className = "";
-        document.getElementById('error-text').innerHTML = error;
-    }
+    document.getElementById('result').className = '';
+    document.getElementById('error').className = 'hidden';
+    document.getElementById('input-unit').innerHTML = inputDefinition.unit;
+    document.getElementById('result-iso').innerHTML = result;
+    document.getElementById('result-epoch').innerHTML = inputDefinition.epoch;
+  } catch (error) {
+    document.getElementById('result').className = 'hidden';
+    document.getElementById('error').className = '';
+    document.getElementById('error-text').innerHTML = error;
+  }
 }
 
 function getInputDefinition(value) {
-    function createInputDefinition(milliseconds, unit, precision) {
-        return {
-            epoch: milliseconds,
-            unit: unit,
-            precision: precision
-        };
-    }
+  function createInputDefinition(milliseconds, unit, precision) {
+    return {
+      epoch: milliseconds,
+      unit: unit,
+      precision: precision
+    };
+  }
 
-    function isEpoch(value) {
-        return value.match(/^[0-9]+$/);
-    }
+  function isEpoch(value) {
+    return value.match(/^[0-9]+$/);
+  }
 
-    let input = {};
+  let input = {};
 
-    if (isEpoch(value)) {
-        if (value.length <= 11) {
-            input = createInputDefinition(Number(value) * 1000, 's', 'epoch');
-        } else if (value.length <= 14) {
-            input = createInputDefinition(Number(value), 'ms', 'epoch');
-        } else if (value.length <= 21) {
-            input = createInputDefinition(Number(value) / 1000000, 'ns', 'epoch');
-        } else {
-            throw Error('Unknown format: number with ' + value.length + ' digits.');
-        }
+  if (isEpoch(value)) {
+    if (value.length <= 11) {
+      input = createInputDefinition(Number(value) * 1000, 's', 'epoch');
+    } else if (value.length <= 14) {
+      input = createInputDefinition(Number(value), 'ms', 'epoch');
+    } else if (value.length <= 21) {
+      input = createInputDefinition(Number(value) / 1000000, 'ns', 'epoch');
     } else {
-        throw Error('Unknown format: ' + value + '.');
+      throw Error('Unknown format: number with ' + value.length + ' digits.');
     }
+  } else {
+    throw Error('Unknown format: ' + value + '.');
+  }
 
-    return input;
+  return input;
 }
 
 function convert(value) {
-    let result;
+  let result;
 
-    let date = new Date(Number(value));
+  let date = new Date(Number(value));
 
-    if (!date.getTime()) {
-        throw Error('Cannot parse to Date.');
-    }
+  if (!date.getTime()) {
+    throw Error('Cannot parse to Date.');
+  }
 
-    result = date.toISOString();
-    return result;
+  result = date.toISOString();
+  return result;
 }
 
-document.addEventListener('input', handleInputEvent)
+document.addEventListener('input', handleInputEvent);
 document.getElementById('input').focus();
